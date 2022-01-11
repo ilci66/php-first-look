14af301d52da824513ea3362bf5ebe01193b2fc4
ilci66 <akbiyik.ilker7@gmail.com>
2022-01-11 20:02:53 +0100

    more notes

---
+++ adv.php
@@ -0,0 +1,9 @@
+<?php 
+// formatting is convenient
+echo "Today is " . date("Y/m/d") . "   ";
+echo "Today is " . date("Y.m.d") . "   ";
+echo "Today is " . date("Y-m-d") . "   ";
+echo "Today is " . date("l") . "   ";
+
+?>
+2010-<?php echo date("Y");?>
